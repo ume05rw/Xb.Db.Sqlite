93dade6b4742256f8aaefb9ced479c0dbaea93c7
ume05rw <info@dobes.jp>
2016-12-20 18:05:41 +0900

    書くだけ書いた。萎えたのでテストは後で。

--- Xb.Db.Sqlite.STD1.3/Properties/AssemblyInfo.cs
+++ Xb.Db.Sqlite.STD1.3/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,31 +7,24 @@ using System.Runtime.InteropServices;
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
 [assembly: AssemblyTitle("Xb.Db.Sqlite")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("Ready to Xamarin & .NET Core, SQLite Access Library.")]
+[assembly: AssemblyConfiguration("Ready to Xamarin & .NET Core, SQLite Access Library.")]
+[assembly: AssemblyCompany("Do-Be's")]
 [assembly: AssemblyProduct("Xb.Db.Sqlite")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright Do-Be's(C)  2016")]
+[assembly: AssemblyTrademark("Do-Be's")]
 [assembly: AssemblyCulture("")]
-
-// ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから 
-// 参照不可能になります。COM からこのアセンブリ内の型にアクセスする場合は、
-// その型の ComVisible 属性を true に設定してください。
-[assembly: ComVisible(false)]
-
-// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
-[assembly: Guid("c8d9bd9b-6156-4968-a5b9-16cdc8c8c2f3")]
+[assembly: NeutralResourcesLanguage("ja")]
 
 // アセンブリのバージョン情報は次の 4 つの値で構成されています:
 //
 //      メジャー バージョン
 //      マイナー バージョン
 //      ビルド番号
-//      Revision
+//      リビジョン
 //
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
---
+++ Xb.Db.Sqlite.STD1.3/Xb/Db/Sqlite.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace Xb.Db
+{
+    /// <summary>
+    /// SQLite用DB接続管理クラス
+    /// </summary>
+    /// <remarks>
+    /// Xb.Dbクラスを継承して実装。
+    /// 主な処理が親クラスに書いてあり、ロジックが行き来するので注意。
+    /// </remarks>
+    public class Sqlite : Xb.Db.DbBase
+    {
+        /// <summary>
+        /// Database-file name
+        /// DBファイル名
+        /// </summary>
+        public string FileName => this.Name;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="password"></param>
+        /// <param name="isBuildModels"></param>
+        /// <param name="additionalString"></param>
+        /// <remarks></remarks>
+        public Sqlite(string fileName
+                    , string password = null
+                    , bool isBuildModels = true
+                    , string additionalString = "")
+            : base(fileName
+                 , ""
+                 , password ?? ""
+                 , ""
+                 , additionalString
+                 , isBuildModels)
+        {
+            this.Init();
+        }
+
+
+        private void Init()
+        {
+            this.Encoding = Encoding.UTF8;
+            this.SqlFind = "SELECT * FROM {0} WHERE {1} LIMIT 1 ";
+        }
+
+
+        /// <summary>
+        /// Connect DB
+        /// DBへ接続する
+        /// </summary>>
+        /// <remarks></remarks>
+        protected override void Open()
+        {
+            //build connection string
+            var connectionString 
+                = string.Format("Version=3;Data Source={0};New=False;Compress=True;{1}",
+                                this.Name,
+                                string.IsNullOrEmpty(this.AdditionalConnectionString)
+                                    ? ""
+                                    : "; " + this.AdditionalConnectionString);
+            try
+            {
+                //connect DB
+                this.Connection = new SqliteConnection(connectionString);
+                this.Connection.Open();
+            }
+            catch (Exception ex)
+            {
+                this.Connection = null;
+                Xb.Util.Out(ex);
+                throw ex;
+            }
+
+            //init transaction
+            this.ResetTransaction(false);
+        }
+
+
+        /// <summary>
+        /// Get Table-Structure
+        /// 接続先DBの構造を取得する。
+        /// </summary>
+        /// <remarks></remarks>
+        protected override void GetStructure()
+        {
+            //get Table list
+            var sql = new System.Text.StringBuilder();
+            sql.AppendFormat(" SELECT ");
+            sql.AppendFormat("     name as TABLE_NAME ");
+            sql.AppendFormat(" FROM ");
+            sql.AppendFormat("     sqlite_master ");
+            sql.AppendFormat(" WHERE ");
+            sql.AppendFormat("     type='table' ");
+            sql.AppendFormat("     and name<>'sqlite_sequence' ");
+            var dt = this.Query(sql.ToString());
+            this.TableNames = dt.Rows.Select(row => row["TABLE_NAME"].ToString()).ToArray();
+
+            var structRows = new List<Xb.Db.DbBase.Structure>();
+
+
+            //テーブル一つずつ、順次カラム情報を追加していく。
+            foreach (string tableName in this.TableNames)
+            {
+                //カラム情報を取得する。
+                sql.Clear();
+                sql.AppendFormat("PRAGMA table_info('{0}') ", tableName);
+                var dt2 = this.Query(sql.ToString());
+                if (dt2 == null
+                    || dt2.Rows.Count <= 0)
+                {
+                    Xb.Util.Out("Xb.Db.Sqlite.GetStructure: カラム情報の取得に失敗しました。");
+                    throw new Exception("Xb.Db.Sqlite.GetStructure: カラム情報の取得に失敗しました。");
+                }
+
+                for (int i = 1; i <= dt2.Rows.Count - 1; i++)
+                {
+                    var row = new Xb.Db.DbBase.Structure();
+                    var typeString = dt2.Rows[i]["type"].ToString().ToLower();
+                    var isNumber = false;
+                    if (typeString.IndexOf("integer", StringComparison.Ordinal) != -1)
+                    {
+                        typeString = "integer";
+                        isNumber = true;
+                    }
+                    else if (typeString.IndexOf("real", StringComparison.Ordinal) != -1)
+                    {
+                        typeString = "real";
+                        isNumber = true;
+                    }
+                    else if (typeString.IndexOf("varchar", StringComparison.Ordinal) != -1)
+                    {
+                        typeString = "varchar";
+                    }
+                    else if (typeString.IndexOf("blob", StringComparison.Ordinal) != -1)
+                    {
+                        typeString = "blob";
+                    }
+                    else if (typeString.IndexOf("datetime", StringComparison.Ordinal) != -1)
+                    {
+                        typeString = "datetime";
+                    }
+                    else if (typeString.IndexOf("boolean", StringComparison.Ordinal) != -1)
+                    {
+                        typeString = "boolean";
+                    }
+                    else if (typeString.IndexOf("none", StringComparison.Ordinal) != -1)
+                    {
+                        typeString = "none";
+                    }
+                    else
+                    {
+                        //typeString = typeString;
+                    }
+
+                    row.TABLE_NAME = tableName;
+                    row.COLUMN_INDEX = (long)dt2.Rows[i]["cid"];
+                    row.COLUMN_NAME  = dt2.Rows[i]["name"].ToString();
+                    row.TYPE = typeString;
+                    row.IS_PRIMARY_KEY = (long)dt2.Rows[i]["pk"];
+                    row.IS_NULLABLE = Convert.ToInt32(dt2.Rows[i]["notnull"]) == 0
+                                             ? 1
+                                             : 0;
+                    row.COMMENT = "";
+
+                    if (isNumber)
+                    {
+                        //数値型のとき
+                        row.CHAR_LENGTH = -1;
+                        row.NUM_PREC = 12;
+                        row.NUM_SCALE = 6;
+                    }
+                    else
+                    {
+                        //文字型、その他のとき
+                        row.CHAR_LENGTH = 65335;
+                        row.NUM_PREC = -1;
+                        row.NUM_SCALE = -1;
+                    }
+
+                    structRows.Add(row);
+                }
+            }
+
+            this.StructureTable = structRows.ToArray();
+        }
+
+
+        /// <summary>
+        /// Get DbParameter object.
+        /// DbParameterオブジェクトを取得する。
+        /// </summary>
+        /// <returns></returns>
+        public DbParameter GetParameter(string name = null
+                                       , object value = null
+                                       , SqliteType type = SqliteType.Text)
+        {
+            if (!string.IsNullOrEmpty(name)
+                && name.Substring(0, 1) != "@")
+                name = "@" + name;
+
+            var param = new SqliteParameter();
+            param.Direction = ParameterDirection.Input;
+            param.ParameterName = name ?? "";
+            param.Value = value;
+            param.SqliteType = type;
+            //param.DbType = type;
+
+            return param;
+        }
+
+
+        /// <summary>
+        /// Get DbCommand object.
+        /// DbCommandオブジェクトを取得する。
+        /// </summary>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        protected override DbCommand GetCommand(DbParameter[] parameters = null)
+        {
+            var result = new SqliteCommand
+            {
+                Connection = (SqliteConnection)this.Connection
+            };
+
+            if (parameters != null
+                && parameters.Length > 0)
+            {
+                result.Parameters.AddRange(parameters);
+            }
+
+            return result;
+        }
+
+
+        /// <summary>
+        /// データベースのバックアップファイルを取得する。
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        /// <remarks></remarks>
+        public override async Task<bool> BackupDbAsync(string fileName)
+        {
+            //渡し値パスが実在することを確認する。
+            if (!await this.RemoveIfExintsAsync(fileName))
+                return false;
+
+            //DBバックアップを実行する。
+            try
+            {
+                //DBファイルをコピーする。
+                System.IO.File.Copy(this.Name, fileName, true);
+            }
+            catch (Exception ex)
+            {
+                Xb.Util.Out("Xb.Db.Sqlite.BackupDb: バックアップファイル取得に失敗しました：" + ex.Message);
+                throw new IOException("Xb.Db.Sqlite.BackupDb: バックアップファイル取得に失敗しました：" + ex.Message);
+            }
+
+            return true;
+        }
+    }
+}
